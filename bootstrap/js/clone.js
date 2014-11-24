28faee444503a5f8955eda4a26a9eee2d9733032
commonsky <commonsky@gmail.com>
2014-11-23 22:49:26 -0500

    fixed left and right panels

--- bootstrap/js/clone.js
+++ bootstrap/js/clone.js
@@ -1,6 +1,7 @@
 $(function(){
 
 	var CompCount = 0;
+	var SchoolCount = 0;
 
 	function checkCompCout() {
 		if(CompCount >= 2){
@@ -9,6 +10,32 @@ $(function(){
 			$('#addcompany').show();
 		}
 	}
+
+	function checkSchoolCout() {
+		if(SchoolCount >= 2){
+			$('#addschool').hide();
+		} else {
+			$('#addschool').show();
+		}
+	}
+
+	$('#addschool a').click(function(event){
+		event.preventDefault();//prevent form submission
+		var formfields = $('.eduform:first-child').clone();
+		$('#eduforms').append(formfields);
+		CompCount++;
+		checkCompCount();
+	});
+
+
+	$('#eduforms').on('click', '.removeschool a', function(event){
+		event.preventDefault();//prevent form submission
+		$(this).parents('.eduform').slideUp(400, function(){
+			$(this).remove();
+		});
+		SchoolCount--;
+		checkSchoolCount();
+	});
 
 	$('#addcompany a').click(function(event){
 		event.preventDefault();//prevent form submission
@@ -19,13 +46,13 @@ $(function(){
 	});
 
 
-	$('#employforms').on('click', '.removecompany a', function(event){
+	$('#eduforms').on('click', '.removeschool a', function(event){
 		event.preventDefault();//prevent form submission
-		$(this).parents('.employform').slideUp(400, function(){
+		$(this).parents('.eduform').slideUp(400, function(){
 			$(this).remove();
 		});
-		CompCount--;
-		checkCompCout();
+		SchoolCount--;
+		checkSchoolCount();
 	});
 
 
