38b91d21df35dc1316de5f20623e58c0965ecef6
commonsky <commonsky@gmail.com>
2014-11-28 23:06:22 -0500

    added video room pages and links. fixed hboxfsect

--- bootstrap/js/clone.js
+++ bootstrap/js/clone.js
@@ -3,7 +3,8 @@ $(function(){
 	var CompCount = 0;
 	var SchoolCount = 0;
 
-	function checkCompCout() {
+
+	function checkCompCount() {
 		if(CompCount >= 2){
 			$('#addcompany').hide();
 		} else {
@@ -11,7 +12,7 @@ $(function(){
 		}
 	}
 
-	function checkSchoolCout() {
+	function checkSchoolCount() {
 		if(SchoolCount >= 2){
 			$('#addschool').hide();
 		} else {
@@ -19,14 +20,38 @@ $(function(){
 		}
 	}
 
-	$('#addschool a').click(function(event){
+	$('#addcompany a').click(function(event){
 		event.preventDefault();//prevent form submission
-		var formfields = $('.eduform:first-child').clone();
-		$('#eduforms').append(formfields);
+		var formfields = $('.employform:first-child').clone();
+		$('#employforms').append(formfields).slideDown(800);
 		CompCount++;
 		checkCompCount();
 	});
 
+	// $('#addcompany a').click(function(){
+	// 	var formfields = $('.employform:first-child').clone();
+	// 	$(formfields,{
+	// 		'class':'hide'
+	// 	}).appendTo('formfields').slideDown(1000);
+	// });
+
+	$('#employforms').on('click', '.removecompany a', function(event){
+		event.preventDefault();//prevent form submission
+		$(this).parents('.employform').slideUp(400, function(){
+			$(this).remove();
+		});
+		CompCount--;
+		checkCompCount();
+	});
+
+	$('#addschool a').click(function(event){
+		event.preventDefault();//prevent form submission
+		var formfields = $('.eduform:first-child').clone();
+		$('#eduforms').append(formfields);
+		SchoolCount++;
+		checkSchoolCount();
+	});
+
 
 	$('#eduforms').on('click', '.removeschool a', function(event){
 		event.preventDefault();//prevent form submission
@@ -37,24 +62,10 @@ $(function(){
 		checkSchoolCount();
 	});
 
-	$('#addcompany a').click(function(event){
-		event.preventDefault();//prevent form submission
-		var formfields = $('.employform:first-child').clone();
-		$('#employforms').append(formfields);
-		CompCount++;
-		checkCompCout();
-	});
-
-
-	$('#eduforms').on('click', '.removeschool a', function(event){
-		event.preventDefault();//prevent form submission
-		$(this).parents('.eduform').slideUp(400, function(){
-			$(this).remove();
-		});
-		SchoolCount--;
-		checkSchoolCount();
-	});
-
-
+	$('#questionbutton').popover();
+	$('#gearbutton').popover();
 
 });
+
+
+
